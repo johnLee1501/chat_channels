101bac1eca50ba669987734e410d32b4bc5f541c
jhonj <johnvega@wposs.com>
2021-06-08 13:56:04 -0500

    deploy heroku

--- chat_channels/asgi.py
+++ chat_channels/asgi.py
@@ -6,7 +6,7 @@ from django.core.asgi import get_asgi_application
 
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_channels.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
